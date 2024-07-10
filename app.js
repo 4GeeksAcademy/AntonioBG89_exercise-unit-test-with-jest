13fcdcd17607b7dbe8ff26312a23bf5557961f25
Antonio <116432648+AntonioBG89@users.noreply.github.com>
2024-07-10 19:40:43 +0000

    Pruebas Unitarias Jest

---
+++ app.js
@@ -0,0 +1,48 @@
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar) {
+    // Convertimos el valor a yenes
+    let valueInYen = valueInDollar * 156.5;
+    // Retornamos el valor en yenes
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInYen) {
+    // Convertimos el valor a pounds
+    let valueInPound = valueInYen * 1.07;
+    // Retornamos el valor en pounds
+    return valueInPound;
+}
+
+
+// Esta es mi función que suma dos números
+const sum = (a,b) => {
+    return a + b
+}
+
+// Solo un registro en consola para nosotros
+console.log(sum(7,3))
+
+// Exporta la función para usarla en otros archivos 
+// (similar a la palabra clave "export" cuando se usa webpack)
+
+// One euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+
+
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
+
+
+
+
